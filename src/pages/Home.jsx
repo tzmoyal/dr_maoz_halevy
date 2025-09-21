e85f2f333c617d3b6a29842786b39e351163f8e9
Tzahi Moyal <tzmoyal@gmail.com>
2025-09-21 13:36:52 +0300

    refactor: remove FAQ and Reviews from home page
    - Remove FAQ component from home page layout
    - Remove Reviews component from home page layout
    - Keep home page focused on core content: Hero, Testimonials, Contact
    - Maintain clean and focused user experience

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Testimonials from '../components/Testimonials';
-import Reviews from '../components/Reviews';
 import Contact from '../components/Contact';
-import FAQ from '../components/FAQ';
 
 export default function Home() {
   return (
     <div className="min-h-screen" id="main">
       <Hero />
       <Testimonials />
-      <Reviews />
-      <FAQ />
       <Contact />
     </div>
   );
